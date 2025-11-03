8d9b7d6a68a7c4e383a359969dec7b917c4fff74
Keveiuz <73086628+Keveiuz@users.noreply.github.com>
2025-11-03 18:55:18 +0800

    Update my_train_csft.sh

--- my_train_csft.sh
+++ my_train_csft.sh
@@ -6,16 +6,16 @@ set -x
 read -r -d '' training_commands <<EOF
 openrlhf.cli.train_sft \
    --max_len 4096 \
-   --dataset /public/home/ljt/zez/Boundary-Sample/experiment-4/train/guardreasoner-100K-in_distribution-csft-4500.parquet \
+   --dataset experiment-4/train/guardreasoner-100K-in_distribution-csft-4500.parquet \
    --input_key question \
    --output_key response \
    --apply_chat_template \
    --train_batch_size 64 \
    --micro_train_batch_size 2 \
    --max_samples 50000 \
-   --pretrain /public/home/ljt/zez/Boundary-Sample/experiment-4/model/guardreasoner-sft-100K \
-   --save_path /public/home/ljt/zez/Boundary-Sample/experiment-4/model/guardreasoner-100K-in_distribution-csft \
-   --ckpt_path /public/home/ljt/zez/Boundary-Sample/experiment-4/model/guardreasoner-100K-in_distribution-csft \
+   --pretrain experiment-4/model/guardreasoner-sft-100K \
+   --save_path experiment-4/model/guardreasoner-100K-in_distribution-csft \
+   --ckpt_path experiment-4/model/guardreasoner-100K-in_distribution-csft \
    --save_steps -1 \
    --max_ckpt_num 3 \
    --save_hf_ckpt \
